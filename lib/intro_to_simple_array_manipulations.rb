d11e1fd9734e6dd3fa866b1c4a62515d954f624e
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-16 00:57:23 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,59 @@
+def using_push(array, string)
+  array.push(string)
+  
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+  
+end
+
+def using_pop(array)
+
+  
+end
+
+def pop_with_args(array)
+
+  
+end
+
+def using_shift(array)
+
+  
+end
+
+def shift_with_args(array)
+
+  
+end
+
+def using_concat(array1,array2)
+
+  
+end
+
+def using_insert(array1, array2, element)
+
+  
+end
+
+def using_uniq(array)
+
+  
+end
+
+def using_flatten(array)
+
+  
+end
+
+def using_delete(array, string)
+
+  
+end
+
+def using_delte_at(array, integar)
+
+  
+end
