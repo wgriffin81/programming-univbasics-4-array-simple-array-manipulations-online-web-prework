8637e06d4f5c021a80c1d935ffcda0721056c5f0
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-16 01:32:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,25 +1,18 @@
 def using_push(array, string)
   array.push(string)
-  
 end
 
 def using_unshift(array, string)
   array.unshift(string)
-  
 end
-
-
 
 def using_pop(array)
   last_item = array.pop
-  
   p last_item
-
 end
 
 def pop_with_args(array)
   last_item = array.pop(2)
-
 end
 
 def using_shift(array)
